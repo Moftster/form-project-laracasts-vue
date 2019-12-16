f4417e80d9f85ba084534e27886327fa0e11de2e
David Moftakhar <david@sellmysnaps.com>
2019-12-16 07:52:06 +0000

    Create page finalised and projects controller setup.

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -15,7 +15,25 @@
 
     <div id="app" class="container">
 
-    
+        @include ('projects.list')
+
+        <form method="POST" action="/projects">
+            <div class="control">
+                <label for="name" class="label">Project Name:</label>
+                <input type="text" id="name" name="name" class="input">
+            </div>
+
+            <div class="control">
+                <label for="description" class="label">Project Description:</label>
+                <input type="text" id="description" name="description" class="input">
+            </div>
+
+            <div class="control">
+                <button class="button is-primary">Create</button>
+            </div>
+        </form>
+
+
 
     </div>
 
